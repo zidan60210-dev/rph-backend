4262892604b98762eafc5746f1c549e152164280
Zidan SK <zidan60210@gmail.com>
2026-07-12 10:46:55 +0530

    RPH backend first version

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const initSqlJs = require("sql.js");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let db;
+
+initSqlJs().then((SQL) => {
+    db = new SQL.Database();
+
+    db.run(`
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER
+        )
+    `);
+
+    db.run("INSERT INTO users (name, age) VALUES ('Zidan SK', 17)");
+
+    app.get("/", (req, res) => {
+        res.send("RPH Backend Running");
+    });
+
+    app.get("/users", (req, res) => {
+        const result = db.exec("SELECT * FROM users");
+        res.json(result);
+    });
+
+    app.listen(3000, () => {
+        console.log("Server running on port 3000");
+    });
+});
+
