b3d61979d8f615cff449f07b34502a94d2de6965
Zidan SK <zidan60210@gmail.com>
2026-07-12 12:33:13 +0530

    Test report API

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.json());
 let db;
 
 initSqlJs().then((SQL) => {
+
     db = new SQL.Database();
 
+
+    // Users Table
     db.run(`
         CREATE TABLE users (
             id INTEGER PRIMARY KEY,
@@ -20,13 +23,34 @@ initSqlJs().then((SQL) => {
         )
     `);
 
-    db.run("INSERT INTO users (name, age) VALUES ('Zidan SK', 17)");
+    db.run(`
+        INSERT INTO users (name, age)
+        VALUES ('Zidan SK', 17)
+    `);
 
+
+    // Reports Table
+    db.run(`
+        CREATE TABLE reports (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            problem TEXT,
+            status TEXT
+        )
+    `);
+
+
+
+    // Home
     app.get("/", (req, res) => {
         res.send("RPH Backend Running");
     });
 
+
+
+    // Get Users
     app.get("/users", (req, res) => {
+
         const result = db.exec("SELECT * FROM users");
 
         const users = result[0].values.map(row => ({
@@ -38,9 +62,62 @@ initSqlJs().then((SQL) => {
         res.json(users);
     });
 
+
+
+    // Submit Report
+    app.post("/reports", (req, res) => {
+
+        const { name, problem } = req.body;
+
+        db.run(
+            "INSERT INTO reports (name, problem, status) VALUES (?, ?, ?)",
+            [
+                name,
+                problem,
+                "Pending"
+            ]
+        );
+
+
+        res.json({
+            message: "Report submitted successfully"
+        });
+
+    });
+
+
+
+    // Get All Reports
+    app.get("/reports", (req, res) => {
+
+        const result = db.exec("SELECT * FROM reports");
+
+
+        if (!result.length) {
+            return res.json([]);
+        }
+
+
+        const reports = result[0].values.map(row => ({
+            id: row[0],
+            name: row[1],
+            problem: row[2],
+            status: row[3]
+        }));
+
+
+        res.json(reports);
+
+    });
+
+
+
     const PORT = process.env.PORT || 3000;
+
 
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
+
+
 });
